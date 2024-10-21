77493f519077f582ed38b9bed61cb97322376e84
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-10-20 18:59:57 -0700

    Add server logging for non-function messages from vapi

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import { Hono } from '@hono/hono'
 // import { cors, prettyJSON } from 'https://deno.land/x/hono@v3.12.8/middleware.ts'
 import { functionCallRoute } from './functions/index.ts'
 import { Bindings } from './types/hono.types.ts'
+import { VapiPayload } from './types/vapi.types.ts'
 
 const app = new Hono<{ Bindings: Bindings }>()
 
@@ -10,7 +11,15 @@ const app = new Hono<{ Bindings: Bindings }>()
 app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
 
 app.get('/', (c) => {
+  console.log('GET / received')
   return c.text('Hello World!')
+})
+
+// Vapi sends messages to this endpoint
+app.post('/', async (c) => {
+  const reqBody = await c.req.json()
+  const payload: VapiPayload = reqBody.message
+  console.log('Vapi payload:', payload)
 })
 
 app.route('/api/functions', functionCallRoute)
